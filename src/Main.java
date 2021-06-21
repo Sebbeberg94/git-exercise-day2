2a4d2f89676eee58b1a52aa847dc1f8d7b5c80ae
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-21 11:16:19 +0200

    lägger till en system.out.prinln grej

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Här ändrar jag någonting för att se vad som händer typ");
 
     }
 
