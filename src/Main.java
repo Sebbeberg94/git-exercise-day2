c5269e5617b5ba735eaf83c0ab24821760c97743
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-21 10:24:39 +0200

    Added interation by lowest value

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> iterateByLowestValue (List<Integer> intList){
+        List<Integer> newList = new ArrayList<>();
+        newList.add(intList.listIterator());
+    }
+}
