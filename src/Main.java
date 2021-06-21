986d8371f082e654856fb8a70461b9b1619ebcdb
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-21 11:20:39 +0200

    tog bort en onödig metod

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Här ändrar jag någonting för att se vad som händer typ");
-
-    }
-
-    public static List<Integer> iterateByLowestValue (List<Integer> intList){
-        List<Integer> newList = new ArrayList<>();
-        newList.add(intList.listIterator());
+        System.out.println("Tog bort en metod som inte gjorde nånting");
     }
 }
