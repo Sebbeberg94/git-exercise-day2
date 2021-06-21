1e67944dacb245eb15357494e91494bd47002d0c
Sebastian Berg <sebastian.berg@hotmail.com>
2021-06-21 11:14:22 +0200

    någon form av ändring

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -9,18 +8,6 @@ public class Main {
 
     public static List<Integer> iterateByLowestValue (List<Integer> intList){
         List<Integer> newList = new ArrayList<>();
-        int lowestSoFar = intList.get(0);
-
-        for (Integer i :
-                intList) {
-            
-        }
-        
-    }
-
-    public static List<Integer> decrementList (List<Integer> intList) {
-        List<Integer> newList = new ArrayList<>();
-        newList.add(Collections.sort(intList));
-        return newList;
+        newList.add(intList.listIterator());
     }
 }
